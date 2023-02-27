4f0202e39d1536282249d5fc0b08f036405bf4f5
Rohan James <rohanjames9282@gmail.com>
2023-02-27 18:24:13 +0530

    Add validations

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,7 @@
 class Kitten < ApplicationRecord
+  validates :name, length: { in: 3..10 }, presence: true
+  validates :age, presence: true
+  validates :cuteness, presence: true
+  validates :softness, presence: true
+
 end
