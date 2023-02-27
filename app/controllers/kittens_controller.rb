40eac8276808f01a7fb9a54dcd8cad079b8680bf
Rohan James <rohanjames9282@gmail.com>
2023-02-27 18:29:52 +0530

    Add #new and #create to kitten controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,18 @@
 class KittensController < ApplicationController
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to root_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  
 end
