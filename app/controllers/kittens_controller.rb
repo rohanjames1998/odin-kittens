9dfbd1587bc13a9f38c9ef0919c33ef1f462bc31
Rohan James <rohanjames9282@gmail.com>
2023-02-28 19:16:54 +0530

    Add #index and #show with views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,5 +1,13 @@
 class KittensController < ApplicationController
 
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
   def new
     @kitten = Kitten.new
   end
